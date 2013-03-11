0dec95ca7c261cae2b8fcc555de539f07a3ae7e9
Urs Breu <dev@gleisdrei.ch>
2013-03-11 14:46:24 +0100

    added focus, postconstruct and predestroy methods

--- com.example.e4.rcp.todo/src/com/example/e4/rcp/todo/parts/PlaygroundPart.java
+++ com.example.e4.rcp.todo/src/com/example/e4/rcp/todo/parts/PlaygroundPart.java
@@ -1,11 +1,28 @@
 package com.example.e4.rcp.todo.parts;
 
-import javax.inject.Inject;
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+import org.eclipse.e4.ui.di.Focus;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Label;
 
 public class PlaygroundPart {
-	@Inject
-	public PlaygroundPart() {
-		// TODO Your code here
+	private Label label;
+
+	@PostConstruct
+	public void createControls(Composite parent) {
+		label = new Label(parent, SWT.NONE);
+		label.setText(getClass().getCanonicalName());
 	}
 
+	@Focus
+	public void onFocus() {
+		label.setFocus();
+	}
+
+	@PreDestroy
+	public void dispose() {
+	}
 }
