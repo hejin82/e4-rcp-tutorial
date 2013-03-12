6004c7587ac5cc6569f7bc54a085be20f7c2ba87
Urs Breu <dev@gleisdrei.ch>
2013-03-12 11:37:37 +0100

    use declarative services and inject the ITodoModel

--- com.example.e4.rcp.todo/src/com/example/e4/rcp/todo/parts/TodoOverviewPart.java
+++ com.example.e4.rcp.todo/src/com/example/e4/rcp/todo/parts/TodoOverviewPart.java
@@ -10,19 +10,18 @@ import org.eclipse.swt.widgets.Label;
 
 import com.example.e4.rcp.todo.addons.Injectable;
 import com.example.e4.rcp.todo.model.ITodoModel;
-import com.example.e4.rcp.todo.service.TodoService;
 
 public class TodoOverviewPart {
 	private Label label;
 
 	@PostConstruct
-	public void createControls(Composite parent, Injectable injectable) {
+	public void createControls(Composite parent, Injectable injectable,
+			ITodoModel model) {
 		label = new Label(parent, SWT.NONE);
 		label.setText(getClass().getCanonicalName());
 
 		injectable.sayHello();
 
-		ITodoModel model = TodoService.getInstance();
 		System.out.println(model.getTodos().size());
 	}
 
