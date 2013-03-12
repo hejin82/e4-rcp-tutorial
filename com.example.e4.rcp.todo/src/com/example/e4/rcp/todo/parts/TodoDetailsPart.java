e6516dc62002f8115971fc4805e4464077ec884f
Urs Breu <dev@gleisdrei.ch>
2013-03-12 09:52:52 +0100

    - added toolbar to details part - iterate over model via model service

--- com.example.e4.rcp.todo/src/com/example/e4/rcp/todo/parts/TodoDetailsPart.java
+++ com.example.e4.rcp.todo/src/com/example/e4/rcp/todo/parts/TodoDetailsPart.java
@@ -1,9 +1,18 @@
 package com.example.e4.rcp.todo.parts;
 
+import java.util.List;
+
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 
 import org.eclipse.e4.ui.di.Focus;
+import org.eclipse.e4.ui.model.application.MApplication;
+import org.eclipse.e4.ui.model.application.MApplicationElement;
+import org.eclipse.e4.ui.model.application.ui.basic.MPart;
+import org.eclipse.e4.ui.model.application.ui.menu.MMenu;
+import org.eclipse.e4.ui.model.application.ui.menu.MToolBar;
+import org.eclipse.e4.ui.model.application.ui.menu.MToolBarElement;
+import org.eclipse.e4.ui.workbench.modeling.EModelService;
 import org.eclipse.e4.ui.workbench.swt.modeling.EMenuService;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Composite;
@@ -15,14 +24,57 @@ public class TodoDetailsPart {
 	private Text text;
 
 	@PostConstruct
-	public void createControls(Composite parent, EMenuService service) {
+	public void createControls(Composite parent, EMenuService service,
+			EModelService modelService, MApplication application, MPart part) {
 		label = new Label(parent, SWT.NONE);
 		label.setText(getClass().getCanonicalName());
 
 		text = new Text(parent, SWT.NONE);
 		text.setText("Foo bar");
-		service.registerContextMenu(text,
-				"com.example.e4.rcp.todo.popupmenu.table");
+
+		List<MMenu> menus = part.getMenus();
+		if (menus.size() == 1) {
+			service.registerContextMenu(text, menus.get(0).getElementId());
+		}
+
+		traverseModel(modelService, application);
+	}
+
+	private void traverseModel(EModelService modelService,
+			MApplication application) {
+		List<MApplicationElement> elements = modelService.findElements(
+				application, null, MApplicationElement.class, null);
+
+		System.out.println("### Application Elements ###");
+		for (MApplicationElement mApplicationElement : elements) {
+			System.out.println(mApplicationElement);
+			System.out.println(mApplicationElement.getElementId());
+		}
+
+		System.out.println("---------");
+		System.out.println("### Parts ###");
+
+		List<MPart> parts = modelService.findElements(application, null,
+				MPart.class, null);
+		for (MPart part : parts) {
+			System.out.println(part);
+			System.out.println(part.getElementId());
+			MToolBar toolbar = part.getToolbar();
+			if (toolbar != null) {
+				List<MToolBarElement> toolbarChildren = toolbar.getChildren();
+				System.out.println("\t toolbar:");
+				for (MToolBarElement mToolBarElement : toolbarChildren) {
+					System.out.println("\t\t" + mToolBarElement.getElementId());
+				}
+			}
+			List<MMenu> menus = part.getMenus();
+			if (menus.size() > 0) {
+				System.out.println("\t menu:");
+			}
+			for (MMenu mMenu : menus) {
+				System.out.println("\t\t" + mMenu.getElementId());
+			}
+		}
 	}
 
 	@Focus
