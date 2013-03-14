9804cb3a8a8fcda9bf3cfb9e41c1a358d6b6895c
Urs Breu <dev@gleisdrei.ch>
2013-03-14 13:59:23 +0100

    load/store preferences

--- com.example.e4.rcp.todo/src/com/example/e4/rcp/todo/parts/PlaygroundPart.java
+++ com.example.e4.rcp.todo/src/com/example/e4/rcp/todo/parts/PlaygroundPart.java
@@ -5,7 +5,10 @@ import java.net.URLEncoder;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
+import javax.inject.Inject;
 
+import org.eclipse.e4.core.di.annotations.Optional;
+import org.eclipse.e4.core.di.extensions.Preference;
 import org.eclipse.e4.ui.di.Focus;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.browser.Browser;
@@ -15,16 +18,24 @@ import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Text;
 
 public class PlaygroundPart {
 	private Text txtCity;
 	private Browser browser;
 	private Button btnSearch;
+	private Label userLabel;
 
 	@PostConstruct
 	public void createControls(final Composite parent) {
 		parent.setLayout(new GridLayout(2, false));
+
+		userLabel = new Label(parent, SWT.NONE);
+		userLabel.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false,
+				1, 1));
+
+		new Label(parent, SWT.NONE);
 
 		txtCity = new Text(parent, SWT.BORDER);
 		txtCity.setToolTipText("Enter City");
@@ -53,6 +64,15 @@ public class PlaygroundPart {
 				}
 			}
 		});
+	}
+
+	@Inject
+	@Optional
+	public void trackUser(
+			@Preference(nodePath = "com.example.e4.rcp.todo", value = "user") String user) {
+		if (userLabel != null && !userLabel.isDisposed()) {
+			userLabel.setText("Hello, " + user);
+		}
 	}
 
 	@Focus
